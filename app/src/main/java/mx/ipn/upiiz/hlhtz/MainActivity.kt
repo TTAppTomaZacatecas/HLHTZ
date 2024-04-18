4046018d1bae638acfc9db4e53419c8af6ee6325
RamEmm <gamumon99@gmail.com>
2024-04-17 18:08:34 -0600

    feat: The GUI design was built

--- app/src/main/java/mx/ipn/upiiz/hlhtz/MainActivity.kt
+++ app/src/main/java/mx/ipn/upiiz/hlhtz/MainActivity.kt
@@ -1,20 +1,34 @@
 package mx.ipn.upiiz.hlhtz
 
 import android.os.Bundle
+import android.os.Message
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var  btn_send: Button
+    private lateinit var et_message: EditText
+    private lateinit var rv_messages : RecyclerView
+
+    var messagesList = mutableListOf<Message>()
+
+    private val botList = listOf("Terminator", "Robocop", "BuzzLigthyear", "Han Solo")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        rv_messages = findViewById(R.id.rv_messages)
+        btn_send = findViewById(R.id.btn_send)
+        et_message = findViewById(R.id.et_message)
+
+
+
+
     }
 }
